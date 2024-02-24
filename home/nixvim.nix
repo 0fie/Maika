576c94347e53f157d102aa983aa04a8c137dd4d1
0fie <151028199+0fie@users.noreply.github.com>
2024-02-24 23:53:26 +0200

    UPDATE home/nixvim.nix - Massively improved Neovim config, added LSP and Tressiter...

--- home/nixvim.nix
+++ home/nixvim.nix
@@ -3,17 +3,82 @@
 {
   programs.nixvim = {
     enable = true;
-    plugins.lualine.enable = true;
+
+    plugins = {
+      lualine.enable = true;
+      neo-tree.enable = true;
+      bufferline.enable = true;
+      notify.enable = true;
+      noice.enable = true;
+
+      dashboard = {
+        enable = true;
+	header = [ "Nixvim 🩵" ];
+	footer = [ "To these types, complex software is the ideal." ];
+	hideTabline = true;
+	hideStatusline = true;
+      };
+
+      treesitter = {
+        enable = true;
+	ensure_installed = [ "nix" "vim" "regex" "lua" "bash" "markdown" "markdown_inline" ];
+	indent= true;
+      };
+      
+      nvim-cmp =  {
+        enable = true;
+	autoEnableSources = true;
+	sources = [
+	  { name = "nvim_lsp"; };
+	  { name = "path"; };
+	  { name = "buffer"; };
+	];
+	mapping = {
+	  "<CR>" = "cmp.mapping.confirm({ select= true })";
+	  "<TAB>" = {
+	    action = ''cmp.mapping.select_next_item()'';
+	    modes = [ "i" "s" ];
+	  };
+	};
+      };
+    };
+
     colorschemes.catppuccin = {
       enable= true;
       flavour = "mocha";
       terminalColors= true;
       transparentBackground= true;
     };
-    options = {
+
+    lsp = {
+      enable = true;
+      servers = {
+        nixd.enable = true;
+      };
+    };
+
+    clipboard.providers.wl-copy.enable = true;
+
+    options = rec {
       number = true;         # Show line numbers
       relativenumber = true; # Show relative line numbers
       shiftwidth = 2;        # Tab width should be 2
+      # clipboard = "unnamedplus";
+      softtabstop = shiftwidth;
+      smartindent = true;
+      swapfile = false;
+      backup = swapfile;
+      scrolloff = 10;
     };
+    
+    keymaps = [
+      {
+        mode = "n";
+        key = "<leader>f";
+        options.silent = false;
+        action = "<cmd>Neotree reveal right<CR>";
+      }
+    ];
+    globals.mapleader = " "; # Sets the leader key to space.
   };
 }
