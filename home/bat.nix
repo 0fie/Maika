f7361d6c92f8bcb604650af2b17aab641a1c8380
0fie <151028199+0fie@users.noreply.github.com>
2024-02-24 16:40:09 +0200

    UPDATE home/bat.nix - Refactored to themes from string to set. String is deprecated

--- home/bat.nix
+++ home/bat.nix
@@ -8,13 +8,15 @@
       theme = "Catppuccin-mocha";
     };
     themes = {
-      Catppuccin-mocha = builtins.readFile (pkgs.fetchFromGitHub {
+      Catppuccin-mocha = {
+        src = pkgs.fetchFromGitHub {
           owner = "catppuccin";
           repo = "bat";
           rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
           sha256 = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
-        }
-        + "/Catppuccin-mocha.tmTheme");
+        };
+        file = "Catppuccin-mocha.tmTheme";
+      };
     };
   };
 }
