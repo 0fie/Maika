7c879fbb4dfafe45aa630842462f080ef3fe3eda
0fie <151028199+0fie@users.noreply.github.com>
2024-02-25 01:43:11 +0200

    CREATE system/services.nix

---
+++ system/services.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }:
+
+{
+  services = {
+    gvfs.enable = true;
+    gnome.gnome-keyring = true;
+    blueman.enable = true;
+    fstrim.enable = true; # Trim SSDs in the background.
+    devmon.enable = true;
+    udisks2.enable = true;
+  };
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk pkgs.xdg-desktop-portal ];
+    configPackages = [ pkgs.xdg-desktop-portal-gtk pkgs.xdg-desktop-portal-hyprland pkgs.xdg-desktop-portal ];
+  };
+}
