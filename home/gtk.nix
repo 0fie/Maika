fdd68bba36f679ed7a79f5b9c3339b7be557eeb7
0fie <151028199+0fie@users.noreply.github.com>
2024-02-25 02:58:34 +0200

    UPDATE home/gtk.nix - Attempt to fix colors

--- home/gtk.nix
+++ home/gtk.nix
@@ -1,6 +1,14 @@
 { config, pkgs, ... }:
 
 {
+  home.pointerCursor = {
+    gtk.enable = true;
+    x11.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Ice";
+    size = 24;
+  };
+
   gtk = {
     enable = true;
     theme = {
