031040a99f6c752f5cc136257c57fc1cf685479c
0fie <151028199+0fie@users.noreply.github.com>
2024-02-25 09:48:54 +0200

    UPDATE home/starship.nix - now fetches config file from github

--- home/starship.nix
+++ home/starship.nix
@@ -3,11 +3,11 @@
 {
   programs.starship = {
     enable = true;
-    settings = lib.importTOML (builtins.readFile (pkgs.fetchFromGitHub {
+    settings = lib.importTOML (pkgs.fetchFromGitHub {
       owner = "0fie";
       repo = "trash";
       rev = "main";
       sha256 = "sha256-4nUN++u8q2W7g5rmkYGEr9rb+fLERsrcXD8daWHw/9U=";
-    } + "starship.toml"));
+    } + "/starship.toml");
   };
 }
