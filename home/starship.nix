a307bf785fadb51de43b14b1edbacd339ac3ab4f
0fie <151028199+0fie@users.noreply.github.com>
2024-02-25 09:40:54 +0200

    UPDATE home/starship.nix - now fetches config file from github

--- home/starship.nix
+++ home/starship.nix
@@ -7,7 +7,7 @@
       owner = "0fie";
       repo = "trash";
       rev = "main";
-      sha256 = "";
+      sha256 = "sha256-4nUN++u8q2W7g5rmkYGEr9rb+fLERsrcXD8daWHw/9U=";
     } + "starship.toml"));
   };
 }
