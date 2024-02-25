0267d58f0a8b363844cd882bd2c76c4d07180339
0fie <151028199+0fie@users.noreply.github.com>
2024-02-25 03:43:06 +0200

    UPDATE home/gtk.nix - Attempt to add cursor theme

--- home/gtk.nix
+++ home/gtk.nix
@@ -20,4 +20,6 @@
     "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
     "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
   };
+
+  home.file.".icons/default".source = "${pkgs.catppuccin-cursors.mochaPink}/share/icons/Catppuccin-Mocha-Pink-Cursors";
 }
