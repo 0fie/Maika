7af72f5006139744d911fe9fa2ca704eefe7c6fa
0fie <151028199+0fie@users.noreply.github.com>
2024-02-25 16:57:19 +0200

    UPDATE - home/hyprland.nix and home/starship.nix - Clean up config file, minor tweaks

--- home/starship.nix
+++ home/starship.nix
@@ -7,7 +7,7 @@
       owner = "0fie";
       repo = "trash";
       rev = "main";
-      sha256 = "";
+      sha256 = "sha256-QAN+60zXKAQurLoRmA3YZFBbv69ZW/Fe8jB1EgkQGB4=";
     } + "/starship.toml");
   };
 }
