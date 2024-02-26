810400dcd706a07a23adc556a990cbcd216c655d
0fie <151028199+0fie@users.noreply.github.com>
2024-02-26 07:20:15 +0200

    UPDATES

--- home/lf.nix
+++ home/lf.nix
@@ -105,6 +105,6 @@
     owner = "0fie";
     repo = "trash";
     rev = "main";
-    sha256 = "sha256-jMGvc/t46e+aMQunYhkqWJqyCmVqAQ27ztyn0noBBsM=";
+    sha256 = "sha256-USUb1b5LOo2n2kMzWgV/o6Xu+t9cnmH7+z/ci3vsrd0=";
   } + "/icons");
 }
