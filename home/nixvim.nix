516603f6a68f82a49ade114232f3eb3549d48d19
0fie <151028199+0fie@users.noreply.github.com>
2024-02-26 11:03:55 +0200

    UPDATE /home/nixvim.nix - added & improved keybinds

--- home/nixvim.nix
+++ home/nixvim.nix
@@ -72,10 +72,10 @@
     clipboard.providers.wl-copy.enable = true;
 
     options = rec {
-      number = true;         # Show line numbers
-      relativenumber = true; # Show relative line numbers
-      shiftwidth = 2;        # Tab width should be 2
-      clipboard = "unnamedplus";
+      number = true;             # Show line numbers
+      relativenumber = true;     # Show relative line numbers
+      shiftwidth = 2;            # Tab width should be 2
+      clipboard = "unnamedplus"; # Sync with OS clipboard
       softtabstop = shiftwidth;
       smartindent = true;
       swapfile = false;
@@ -88,23 +88,22 @@
       {
         mode = "n";
         key = "<leader>f";
-        # options.silent = false;
         action = "<cmd>Neotree toggle<CR>";
       }
       {
 	mode = "n";
 	key = "A-l";
-	action = "tabnext";
+	action = "<cmd>tabnext<CR>";
       }
       {
 	mode = "n";
 	key = "A-h";
-	action = "tabprevious";
+	action = "<cmd>tabprevious<CR>";
       }
       {
 	mode = "n";
 	key = "A-q";
-	action = "tabclose";
+	action = "<cmd>q<CR>";
       }
     ];
     globals.mapleader = " "; # Sets the leader key to space.
