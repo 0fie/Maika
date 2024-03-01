e7881a1939b4d5be123eae6af52136360f1c01e1
0fie <151028199+0fie@users.noreply.github.com>
2024-03-01 20:44:48 +0200

    added 27 more mime types 🚀

--- home/xdg.nix
+++ home/xdg.nix
@@ -1,6 +1,12 @@
-# TODO: Add more mime types. Make loupe the default image viewer using Aylurs solution.
-{ config, lib, pkgs, ... }:
+{ config, pkgs, ... }:
 
+let
+  browser = "firefox.desktop";
+  docOpener = "zathura.desktop";
+  imageViewer = "org.gnome.Loupe.desktop";
+  fileManager = "org.gnome.Nautilus.desktop";
+  torrentClient = "transmission-gtk.desktop";
+in
 {
   home.packages = with pkgs; [ xdg-utils ];
   xdg = {
@@ -16,9 +22,41 @@
       enable = true;
       associations.added = defaultApplications;
       defaultApplications = {
-        "text/html" = [ "firefox.desktop" ];
-	"application/pdf" = [ "firefox.desktop" ];
-	"inode/directory" = [ "org.gnome.Nautilus.desktop" ];
+	"application/pdf" = [ docOpener ];
+	"inode/directory" = [ fileManager ];
+	"x-scheme-handler/magnet" = torrentClient;
+
+	# Web stuff
+	"text/html" = [ browser ];
+	"x-scheme-handler/http" = browser;
+	"x-scheme-handler/https" = browser;
+	"application/xhtml+xml" = browser;
+
+        # Images
+        "image/jpeg" = imageViewer;
+        "image/bmp" = imageViewer;
+        "image/gif" = imageViewer;
+        "image/jpg" = imageViewer;
+        "image/pjpeg" = imageViewer;
+        "image/png" = imageViewer;
+        "image/tiff" = imageViewer;
+        "image/webp" = imageViewer;
+        "image/x-bmp" = imageViewer;
+        "image/x-gray" = imageViewer;
+        "image/x-icb" = imageViewer;
+        "image/x-ico" = imageViewer;
+        "image/x-png" = imageViewer;
+        "image/x-portable-anymap" = imageViewer;
+        "image/x-portable-bitmap" = imageViewer;
+        "image/x-portable-graymap" = imageViewer;
+        "image/x-portable-pixmap" = imageViewer;
+        "image/x-xbitmap" = imageViewer;
+        "image/x-xpixmap" = imageViewer;
+        "image/x-pcx" = imageViewer;
+        "image/svg+xml" = imageViewer;
+        "image/svg+xml-compressed" = imageViewer;
+        "image/vnd.wap.wbmp" = imageViewer;
+        "image/x-icns" = imageViewer;
       };
     };
   };
