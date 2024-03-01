5bee927e505f43f065e80ea52fbfe7b850d388ff
0fie <151028199+0fie@users.noreply.github.com>
2024-03-01 09:32:06 +0200

    omg we have hyprlock now 🔥

--- home/hyprlock.nix
+++ home/hyprlock.nix
@@ -1,3 +1,57 @@
-{ pkgs, config, ... }:
+{ config, pkgs, ... }:
 
-{}
+{
+  programs.hyprlock = {
+    enable = true;
+    general = {
+      hide_cursor = true;
+    };
+
+    backgrounds = [
+      {
+        monitor = "";
+        path = toString( pkgs.fetchurl {
+	  url = "https://raw.githubusercontent.com/0fie/wallpapers/main/Art/mountain.png";
+	  sha256 = "sha256-8LmAIexewsIDpTg8ijm5dnhV+477kFAG8e7FeqEvh0Y=";
+	});
+      }
+    ];
+
+    input-fields = [
+      {
+        size = {
+          width = 300;
+          height = 50;
+      };
+
+        outline_thickness = 2;
+
+        outer_color = "rgb(000000)";
+        inner_color = "rgb(200, 200, 200)";
+        font_color = "rgb(10, 10, 10)";
+        fade_on_empty = false;
+        placeholder_text = "password...";
+        dots_spacing = 0.3;
+        dots_center = true;
+      }
+    ];
+
+    labels = [
+      {
+        monitor = "";
+        text = "$TIME";
+	font_family = "JetBrains Mono Nerd Font";
+        font_size = 50;
+        color = "rgb(200, 200, 200)";
+
+        position = {
+          x = 0;
+          y = 80;
+        };
+
+        valign = "center";
+        halign = "center";
+      }
+    ];
+  };
+}
