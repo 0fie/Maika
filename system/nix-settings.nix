005043771f8ed1201d106692afcb5d859ce69633
0fie <151028199+0fie@users.noreply.github.com>
2024-03-01 07:04:21 +0200

    added cachix for hyprland

--- system/nix-settings.nix
+++ system/nix-settings.nix
@@ -6,6 +6,13 @@
       auto-optimise-store = true;
       trusted-users = [ "root" "me" ];
       experimental-features = [ "nix-command" "flakes" ];
+      substituters = [
+        "https://cache.nixos.org/"
+        "https://hyprland.cachix.org"
+      ];
+      trusted-public-keys = [
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      ];
     };
 
     # Enable auto cleanup.
