4036d0b144de57d16b051c19c202933e08aebfeb
0fie <151028199+0fie@users.noreply.github.com>
2024-03-02 14:29:44 +0200

    dont warn me about dirty git repos 🤬

--- system/nix-settings.nix
+++ system/nix-settings.nix
@@ -12,6 +12,7 @@
       trusted-public-keys = [
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
+      warn-dirty = false;
     };
 
     # Enable auto cleanup.
