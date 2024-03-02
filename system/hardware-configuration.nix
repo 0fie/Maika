788db7da891a7ba3ea376c48df3b057cd8b05ef5
0fie <151028199+0fie@users.noreply.github.com>
2024-03-02 22:07:24 +0200

    tmpfs for /tmp is now safely declared in system/sys.nix - we no longer need to manually edit the auto-gened hardware-configuration file

--- system/hardware-configuration.nix
+++ system/hardware-configuration.nix
@@ -13,11 +13,11 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  # I already modified this in /system/sys.nix 👇️
-  # fileSystems."/" =
-  #   { device = "/dev/disk/by-uuid/f554d441-3e59-452a-bad7-e1cb9ef2fef5";
-  #     fsType = "ext4";
-  #   };
+  # tmpfs for /tmp is safely declared in system/sys.nix
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/f554d441-3e59-452a-bad7-e1cb9ef2fef5";
+      fsType = "ext4";
+    };
 
   swapDevices = [ ];
 
--- system/sys.nix
+++ system/sys.nix
@@ -13,11 +13,6 @@
   };
 
   fileSystems = {
-    "/" = { 
-      device = "/dev/disk/by-uuid/f554d441-3e59-452a-bad7-e1cb9ef2fef5";
-      fsType = "ext4";
-    };
-
     "/tmp" = { 
       fsType = "tmpfs";
     };
