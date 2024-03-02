7e4580ff4c4e037f9e61226c5983ef8aebfd803b
0fie <151028199+0fie@users.noreply.github.com>
2024-03-02 21:26:49 +0200

    added Neovim desktop entry

--- home/xdg.nix
+++ home/xdg.nix
@@ -60,4 +60,34 @@ in
       };
     };
   };
+
+  xdg.desktopEntries = {
+    nvim = {
+      name = "Neovim";
+      genericName = "Text Editor";
+      comment = "Edit text files";
+      exec = "${pkgs.kitty}/bin/kitty -e nvim %F";
+      icon = "nvim";
+      mimeType = [
+        "text/english"
+        "text/plain"
+        "text/x-makefile"
+        "text/x-c++hdr"
+        "text/x-c++src"
+        "text/x-chdr"
+        "text/x-csrc"
+        "text/x-java"
+        "text/x-moc"
+        "text/x-pascal"
+        "text/x-tcl"
+        "text/x-tex"
+        "application/x-shellscript"
+        "text/x-c"
+        "text/x-c++"
+      ];
+      terminal = true;
+      type = "Application";
+      categories = [ "Utility" "TextEditor" ];
+    };
+  };
 }
