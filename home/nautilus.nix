d7df169d8e6c775a2a17abcb993cb5e5844f42ba
0fie <151028199+0fie@users.noreply.github.com>
2024-03-02 17:01:28 +0200

    temporarily commented everything out, testing

--- home/nautilus.nix
+++ home/nautilus.nix
@@ -2,4 +2,15 @@
 
 {
   home.packages = with pkgs; [ gnome.nautilus ];
+  dconf.settings = {
+    "org/gnome/desktop/peripherals/touchpad" = {
+      tap-to-click = true;
+      two-finger-scrolling-enabled = true;
+    };
+  };
+
+  #programs.nautilus-open-any-terminal = {
+  #  enable = true;
+  #  terminal = "kitty";
+  #};
 }
