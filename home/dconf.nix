e1baa37e4861e1e1e6d268feb85fffb34eedc422
0fie <151028199+0fie@users.noreply.github.com>
2024-03-02 16:58:27 +0200

    temporarily commented everything out, testing

--- home/dconf.nix
+++ home/dconf.nix
@@ -3,18 +3,13 @@
 # with lib.hm.gvariant;
 {
   dconf.settings = {
-    "org/gnome/desktop/peripherals/touchpad" = {
-      tap-to-click = true;
-      two-finger-scrolling-enabled = true;
-    };
+    #"com/github/stunkymonkey/nautilus-open-any-terminal" = {
+    #  terminal = "kitty";
+    #};
 
-    "com/github/stunkymonkey/nautilus-open-any-terminal" = {
-      terminal = "kitty";
-    };
-
-    "org/virt-manager/virt-manager/connections" = {
-      autoconnect = ["qemu:///system"];
-        uris = ["qemu:///system"];
-    };
+    # "org/virt-manager/virt-manager/connections" = {
+    #   autoconnect = ["qemu:///system"];
+    #     uris = ["qemu:///system"];
+    # };
   };
 }
