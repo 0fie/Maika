450f4eb5b1d5f1e64b838ea119867dc651f388e7
0fie <151028199+0fie@users.noreply.github.com>
2024-03-03 22:57:53 +0200

    configured PAM for hyprlock

--- system/users.nix
+++ system/users.nix
@@ -13,7 +13,7 @@
     };
   };
 
-  # Required by Nautilus
+  # Required by Nautilus. Does not work though.
   programs.nautilus-open-any-terminal = {
     enable = true;
     terminal = "kitty";
@@ -22,7 +22,11 @@
   # This is required for ZSH to function properly for this user. Removing it might prevent the user from logging in.
   programs.zsh.enable = true;
 
-  # The line below enable crucial system components necessary for Hyprland to run properly. If you remove it, Hyprland WILL crash
+  # The line below enables crucial system components necessary for Hyprland to run properly. If you remove it, Hyprland WILL crash
   # whenever you attempt to start it.
   programs.hyprland.enable = true;
+
+  # This is required by Hyprlock. The package installed through home-manager will *not* be able to unlock the session
+  # without this configuration.
+  security.pam.services.hyprlock = {};
 }
