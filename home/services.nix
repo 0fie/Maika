068dc67231ca48ac3da2a2f72c65341af3242676
0fie <151028199+0fie@users.noreply.github.com>
2024-03-03 09:39:32 +0200

    minor tweaks - hypridle

--- home/services.nix
+++ home/services.nix
@@ -5,12 +5,17 @@
     network-manager-applet.enable = true;
     blueman-applet.enable= true;
 
-    hypridle = {
+    hypridle = rec {
       enable = true;
+      lockCmd = "hyprlock";
+      unlockCmd = "notify-send 'Welcome back! 🙂'";
+      beforeSleepCmd = "notify-send ''";
+      afterSleepCmd = "notify-send ''";
       ignoreDbusInhibit = false;
+
       listeners = [{
           timeout = 600;
-          onTimeout = "hyprlock & sleep 600; systemctl suspend";
+          onTimeout = "${lockCmd} & sleep 600; systemctl suspend";
           onResume = "notify-send 'Welcome back! 🙂'";
       }];
     };
