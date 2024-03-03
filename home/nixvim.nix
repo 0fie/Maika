032ce64372e21d57483b9dc3e8787bcc50d92e2b
0fie <151028199+0fie@users.noreply.github.com>
2024-03-03 12:32:55 +0200

    minor tweaks

--- home/nixvim.nix
+++ home/nixvim.nix
@@ -16,7 +16,7 @@
       cmap w!! w !sudo tee > /dev/null %
     '';
 
-    extraConfigLua = /* lua */''
+    extraConfigLua = /* lua */ ''
       vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
       vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
