4e5041416192b9ed391409f3272b2828c1c58e0f
0fie <151028199+0fie@users.noreply.github.com>
2024-03-04 08:01:46 +0200

    minor tweaks

--- system/nix-settings.nix
+++ system/nix-settings.nix
@@ -6,21 +6,19 @@
       auto-optimise-store = true;
       trusted-users = [ "root" "me" ];
       experimental-features = [ "nix-command" "flakes" ];
-      substituters = [
-        "https://hyprland.cachix.org"
-      ];
-      trusted-public-keys = [
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      ];
       warn-dirty = false;
+
+      # You don't wanna compile Hyprland from source, do you?
+      substituters = ["https://hyprland.cachix.org"];
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
     };
 
     # Enable auto cleanup.
     gc = {
       automatic = true;
       persistent = true;
-      dates = "weekly";
-      options = "--delete-older-than 14d";
+      dates = "daily";
+      options = "--delete-older-than 1d";
     };
   };
 
