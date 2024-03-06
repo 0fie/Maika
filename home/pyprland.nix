7ed50cbbe11e2b3548691f8bd6beddc5f01a9060
0fie <151028199+0fie@users.noreply.github.com>
2024-03-06 15:56:07 +0200

    added pyprland for scratchpads and more!

---
+++ home/pyprland.nix
@@ -0,0 +1,39 @@
+{
+  xdg.configFile."hypr/pyprland.toml".text = ''
+    [pyprland]
+    plugins = [
+      "toggle_special",
+      "scratchpads",
+    ]
+
+    [workspaces_follow_focus]
+    max_workspaces = 9
+
+    [expose]
+    include_special = false
+
+    [scratchpads.term]
+    animation = "fromTop"
+    command = "kitty --class kitty-dropterm"
+    class = "kitty-dropterm"
+    size = "75% 60%"
+    max_size = "1920px 100%"
+
+    [scratchpads.volume]
+    animation = "fromRight"
+    command = "pavucontrol"
+    class = "pavucontrol"
+    lazy = true
+    size = "40% 90%"
+    max_size = "1080px 100%"
+    unfocus = "hide"
+
+    [layout_center]
+    margin = 60
+    offset = [0, 30]
+    next = "movefocus r"
+    prev = "movefocus l"
+    next2 = "movefocus d"
+    prev2 = "movefocus u"
+  '';
+}
