7239c5e84c06c7c3264483a1e258f93af2a9f009
0fie <151028199+0fie@users.noreply.github.com>
2024-03-06 10:34:06 +0200

    added hyprland-plugins flake

--- flake.nix
+++ flake.nix
@@ -1,43 +1,10 @@
 {
   description = "0fie's ultra-simple NixOS Configuration";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    hyprland = { url = "github:hyprwm/Hyprland"; };
-
-    nix-colors = { url = "github:misterio77/nix-colors"; };
-
-    firefox-addons = {
-      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    hyprlock = {
-      url = "github:hyprwm/hyprlock";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    hypridle = {
-      url = "github:hyprwm/hypridle";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    nixvim = {
-      url = "github:nix-community/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
-
   outputs = { self, nixpkgs, home-manager, ... }@inputs:
     let
       system = "x86_64-linux";
-      info = (import ./options.nix);
+      OPT = (import ./options.nix);
 
       pkgs = import nixpkgs {
         inherit system;
@@ -45,7 +12,7 @@
       };
     in {
       nixosConfigurations = {
-        "NixOS" = nixpkgs.lib.nixosSystem {
+        OPT.system.hostName = nixpkgs.lib.nixosSystem {
           specialArgs = {
             inherit system;
             inherit inputs;
@@ -59,15 +26,50 @@
               home-manager.extraSpecialArgs = {
                 # inherit userName;
                 # inherit inputs;
-                inherit info;
+                inherit OPT;
               };
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.backupFileExtension = "backup";
-              home-manager.users.${info.user.userName} = import ./home/home.nix;
+              home-manager.users.${OPT.user.userName} = import ./home/home.nix;
             }
           ];
         };
       };
     };
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-colors = { url = "github:misterio77/nix-colors"; };
+
+    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      inputs.hyprland.follows = "hyprland";
+    };
+    hyprlock = {
+      url = "github:hyprwm/hyprlock";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hypridle = {
+      url = "github:hyprwm/hypridle";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 }
