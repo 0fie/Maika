dce3934d83068979ef396b64879b67550707e3da
0fie <151028199+0fie@users.noreply.github.com>
2024-03-06 22:13:14 +0200

    locale and timezone vars are now fetched from options.nix

--- system/locale.nix
+++ system/locale.nix
@@ -1,26 +1,30 @@
 { config, ... }:
 
-{
+let inherit (import ./options.nix) theTimezone theLocale theLCVariables;
+in {
   # Select internationalisation properties.
   i18n = {
-    defaultLocale = "en_ZW.UTF-8";
+    # Set your time zone
+    time.timeZone = "${theTimezone}";
 
-    extraLocaleSettings = {
-      LC_ADDRESS = "en_ZW.UTF-8";
-      LC_IDENTIFICATION = "en_ZW.UTF-8";
-      LC_MEASUREMENT = "en_ZW.UTF-8";
-      LC_MONETARY = "en_ZW.UTF-8";
-      LC_NAME = "en_ZW.UTF-8";
-      LC_NUMERIC = "en_ZW.UTF-8";
-      LC_PAPER = "en_ZW.UTF-8";
-      LC_TELEPHONE = "en_ZW.UTF-8";
-      LC_TIME = "en_ZW.UTF-8";
+    # Select internationalisation properties
+    i18n.defaultLocale = "${theLocale}";
+    i18n.extraLocaleSettings = {
+      LC_ADDRESS = "${theLCVariables}";
+      LC_IDENTIFICATION = "${theLCVariables}";
+      LC_MEASUREMENT = "${theLCVariables}";
+      LC_MONETARY = "${theLCVariables}";
+      LC_NAME = "${theLCVariables}";
+      LC_NUMERIC = "${theLCVariables}";
+      LC_PAPER = "${theLCVariables}";
+      LC_TELEPHONE = "${theLCVariables}";
+      LC_TIME = "${theLCVariables}";
     };
   };
 
   # Configure your time settings.
   time = {
-    timeZone = "Africa/Harare";
+    timeZone = "${theTimezone}/Harare";
     hardwareClockInLocalTime = true;
   };
 }
