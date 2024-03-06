1cd6474b4e11ffbc29c929f0ea129a9dd8af7e37
0fie <151028199+0fie@users.noreply.github.com>
2024-03-06 15:08:52 +0200

    added lspkind

--- home/nixvim.nix
+++ home/nixvim.nix
@@ -23,6 +23,7 @@
       nvim-colorizer.enable = true;
       nvim-lightbulb.enable = true;
       rainbow-delimiters.enable = true;
+      lspkind.enable = true;
 
       gitsigns.enable = true;
       telescope = {
@@ -179,21 +180,21 @@
         vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
         vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-         vim.keymap.set({ "n", "i", "v" }, "<A-l>", vim.cmd.bnext, { desc = "Switch to next Buffer" })
-         vim.keymap.set({ "n", "i", "v" }, "<A-h>", vim.cmd.bprev, { desc = "Switch to prev Buffer" })
-         vim.keymap.set("n", "<C-q>", function() vim.cmd("bw"); end, { desc = "Close Buffer" })
+        vim.keymap.set({ "n", "i", "v" }, "<A-l>", vim.cmd.bnext, { desc = "Switch to next Buffer" })
+        vim.keymap.set({ "n", "i", "v" }, "<A-h>", vim.cmd.bprev, { desc = "Switch to prev Buffer" })
+        vim.keymap.set("n", "<C-q>", function() vim.cmd("bw"); end, { desc = "Close Buffer" })
 
-         vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
-         vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
-         vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
-         vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover Documentation" })
-         vim.keymap.set("n", "<space>a", vim.lsp.buf.code_action, { desc = "Code action" })
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover Documentation" })
+        vim.keymap.set("n", "<space>a", vim.lsp.buf.code_action, { desc = "Code action" })
 
-         vim.keymap.set("n", "<space>d", vim.diagnostic.open_float, { desc = "Floating diagnostic" })
-         vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
-         vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
-         vim.keymap.set("n", "gl", vim.diagnostic.setloclist, { desc = "Diagnostics on loclist" })
-         vim.keymap.set("n", "gq", vim.diagnostic.setqflist, { desc = "Diagnostics on quickfix" })
+        vim.keymap.set("n", "<space>d", vim.diagnostic.open_float, { desc = "Floating diagnostic" })
+        vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
+        vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
+        vim.keymap.set("n", "gl", vim.diagnostic.setloclist, { desc = "Diagnostics on loclist" })
+        vim.keymap.set("n", "gq", vim.diagnostic.setqflist, { desc = "Diagnostics on quickfix" })
       '';
   };
 }
