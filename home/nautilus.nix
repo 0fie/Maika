b7b7c601dd0e840825aa92ea98ff310397f01a73
0fie <151028199+0fie@users.noreply.github.com>
2024-03-07 12:33:48 +0200

    gstreamer declaration and session vars are now declared in home/nautilus

--- home/nautilus.nix
+++ home/nautilus.nix
@@ -1,8 +1,19 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
-  home.packages = with pkgs; [ gnome.nautilus ];
+  home = {
+    packages = with pkgs; [ gnome.nautilus gst_all_1.gstreamer ];
 
+    # These session variables allow Nautilus to display audio/video properties of a media file.
+    sessionVariables = {
+      GST_PLUGIN_SYSTEM_PATH_1_0 =
+        lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" [
+          pkgs.gst_all_1.gst-plugins-good
+          pkgs.gst_all_1.gst-plugins-bad
+          pkgs.gst_all_1.gst-plugins-ugly
+        ];
+    };
+  };
   dconf.settings = {
     "org/gnome/desktop/peripherals/touchpad" = {
       tap-to-click = true;
