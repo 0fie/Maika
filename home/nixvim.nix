8686fa65a6112f60a5de5a78831b2b50d4362ef5
0fie <151028199+0fie@users.noreply.github.com>
2024-03-08 11:56:02 +0200

    Nixvim now works... happy, happy, happy, happy, happy, happy, happy, happy, happy, happy, happy, happy, happy, happy

--- home/nixvim.nix
+++ home/nixvim.nix
@@ -92,7 +92,36 @@
 
       cmp = {
         enable = true;
-        autoEnableSources = true;
+
+        settings = {
+          autoEnableSources = true;
+
+          sources = [
+            { name = "nvim_lsp"; }
+            { name = "path"; }
+            { name = "buffer"; }
+            { name = "luasnip"; }
+          ];
+
+          snippet.expand = ''
+            function(args)
+              require "luasnip".lsp_expand(args.body)
+            end
+          '';
+
+          mapping = {
+            "<C-Space>" = "cmp.mapping.complete()";
+            "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+            "<C-e>" = "cmp.mapping.close()";
+            "<C-f>" = "cmp.mapping.scroll_docs(4)";
+            "<CR>" = "cmp.mapping.confirm({ select = true })";
+            "<S-Tab>" =
+              "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+            "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+          };
+
+          mappingPresets = [ "insert" ];
+        };
       };
     };
 
