d9ed24669579182bf9658cec926a602b30810eb5
0fie <151028199+0fie@users.noreply.github.com>
2024-03-08 07:13:29 +0200

    nvim-cmp is now cmp, maintainers are idiots!

--- home/nixvim.nix
+++ home/nixvim.nix
@@ -90,18 +90,9 @@
         nixGrammars = true;
       };
 
-      nvim-cmp = {
+      cmp = {
         enable = true;
         autoEnableSources = true;
-        sources =
-          [ { name = "nvim_lsp"; } { name = "path"; } { name = "buffer"; } ];
-        mapping = {
-          "<CR>" = "cmp.mapping.confirm({ select= true })";
-          "<TAB>" = {
-            action = "cmp.mapping.select_next_item()";
-            modes = [ "i" "s" ];
-          };
-        };
       };
     };
 
@@ -196,6 +187,74 @@
         vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
         vim.keymap.set("n", "gl", vim.diagnostic.setloclist, { desc = "Diagnostics on loclist" })
         vim.keymap.set("n", "gq", vim.diagnostic.setqflist, { desc = "Diagnostics on quickfix" })
+
+        local cmp = require'cmp'
+
+        cmp.setup({
+          snippet = {
+            -- REQUIRED - you must specify a snippet engine
+            expand = function(args)
+              vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+              -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+              -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+              -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+            end,
+          },
+          window = {
+            -- completion = cmp.config.window.bordered(),
+            -- documentation = cmp.config.window.bordered(),
+          },
+          mapping = cmp.mapping.preset.insert({
+            ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+            ['<C-f>'] = cmp.mapping.scroll_docs(4),
+            ['<C-Space>'] = cmp.mapping.complete(),
+            ['<C-e>'] = cmp.mapping.abort(),
+            ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          }),
+          sources = cmp.config.sources({
+            { name = 'nvim_lsp' },
+            { name = 'vsnip' }, -- For vsnip users.
+            -- { name = 'luasnip' }, -- For luasnip users.
+            -- { name = 'ultisnips' }, -- For ultisnips users.
+            -- { name = 'snippy' }, -- For snippy users.
+          }, {
+            { name = 'buffer' },
+          })
+        })
+
+        -- Set configuration for specific filetype.
+        cmp.setup.filetype('gitcommit', {
+          sources = cmp.config.sources({
+            { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+          }, {
+            { name = 'buffer' },
+          })
+        })
+
+        -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+        cmp.setup.cmdline({ '/', '?' }, {
+          mapping = cmp.mapping.preset.cmdline(),
+          sources = {
+            { name = 'buffer' }
+          }
+        })
+
+        -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+        cmp.setup.cmdline(':', {
+          mapping = cmp.mapping.preset.cmdline(),
+          sources = cmp.config.sources({
+            { name = 'path' }
+          }, {
+            { name = 'cmdline' }
+          })
+        })
+
+        -- Set up lspconfig.
+        local capabilities = require('cmp_nvim_lsp').default_capabilities()
+        -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+        require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
+          capabilities = capabilities
+        }
       '';
   };
 }
