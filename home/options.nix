3ad2a6da110420091dc02223c967442c831cd692
0fie <151028199+0fie@users.noreply.github.com>
2024-03-09 16:46:25 +0200

    user and system otions are now in separate directories, again

---
+++ home/options.nix
@@ -0,0 +1,8 @@
+{
+  # User options.
+  userName = "me";
+  userFullName = "0fie";
+  gitUserName = "0fie";
+  gitEmail = "151028199+0fie@users.noreply.github.com";
+  dotfilesDir = "/home/me/Projects/Dotfiles";
+}
---
+++ system/options.nix
@@ -0,0 +1,8 @@
+{
+  # System options.
+  hostName = "NixOS";
+  theLocale = "en_US.UTF-8";
+  theLCVariables = "en_ZW.UTF-8";
+  theTimezone = "Africa/Harare";
+  system = "X86_64-linux";
+}
