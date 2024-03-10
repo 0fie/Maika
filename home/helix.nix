224a4f118a57fef5856e884a5bb28f0bc6bd09fd
0fie <151028199+0fie@users.noreply.github.com>
2024-03-10 09:03:48 +0200

    Helix to replace Neovim?

---
+++ home/helix.nix
@@ -0,0 +1,77 @@
+{ pkgs, ... }:
+
+{
+  programs.helix = {
+    enable = true;
+
+    extraPackages = with pkgs;
+      with nodePackages; [
+        vscode-langservers-extracted
+        typescript
+        typescript-language-server
+        marksman
+        nil
+        nixpkgs-fmt
+        clang-tools
+        bash-language-server
+      ];
+
+    languages.language = [
+      {
+        name = "typescript";
+        indent.tab-width = 4;
+        indent.unit = " ";
+        auto-format = true;
+      }
+      {
+        name = "javascript";
+        indent.tab-width = 4;
+        indent.unit = " ";
+        auto-format = true;
+      }
+      {
+        name = "nix";
+        formatter.command = "nixpkgs-fmt";
+      }
+    ];
+
+    settings = {
+      keys = {
+        normal = {
+          C-s = ":w";
+          C-q = ":bclose";
+          A-l = "goto_next_buffer";
+          A-h = "goto_previous_buffer";
+        };
+      };
+
+      theme = "base16_transparent";
+
+      editor = {
+        line-number = "relative";
+        completion-trigger-len = 1;
+        bufferline = "multiple";
+        color-modes = true;
+        statusline = {
+          left = [
+            "mode"
+            "spacer"
+            "diagnostics"
+            "version-control"
+            "file-name"
+            "read-only-indicator"
+            "file-modification-indicator"
+            "spinner"
+          ];
+          right = [ "file-encoding" "file-type" "selections" "position" ];
+        };
+        cursor-shape.insert = "bar";
+        whitespace.render.tab = "all";
+        indent-guides = {
+          render = true;
+          character = "┊";
+        };
+      };
+    };
+  };
+}
