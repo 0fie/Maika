d53646885d3948ba42923f17626c4a2b5711f84e
0fie <151028199+0fie@users.noreply.github.com>
2024-03-11 06:09:55 +0200

    moved cachix stuff to /flake.nix

--- system/nix-settings.nix
+++ system/nix-settings.nix
@@ -10,12 +10,6 @@ in {
       trusted-users = [ "root" "${userName}" ];
       experimental-features = [ "nix-command" "flakes" ];
       warn-dirty = false;
-
-      # You don't wanna compile Hyprland from source, do you?
-      substituters = [ "https://hyprland.cachix.org" ];
-      trusted-public-keys = [
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      ];
     };
 
     # Enable auto cleanup.
