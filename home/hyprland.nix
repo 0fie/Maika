7accd4eedcecd75a25209e3771aee5ba5c99d440
0fie <151028199+0fie@users.noreply.github.com>
2024-03-11 05:44:06 +0200

    what?

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -83,7 +83,6 @@ in {
           "border, 1, 10, default"
           "borderangle, 1, 8, default"
           "fade, 1, 7, default"
-          #"workspaces, 1, 8, default, slidefadevert"
           "workspaces, 1, 7, myBezier, slidefadevert"
         ];
       };
