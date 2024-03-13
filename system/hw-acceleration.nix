7dcfc9031d3aa4c2088b9e0a4c34be4e746d8e0f
0fie <151028199+0fie@users.noreply.github.com>
2024-03-13 14:49:57 +0200

    added hardware acceleration

--- system/hw-acceleration.nix
+++ system/hw-acceleration.nix
@@ -1,6 +1,10 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
+
   hardware = {
     bluetooth = {
       enable = true;
@@ -9,6 +13,15 @@
 
     opengl = {
       enable = true;
+      extraPackages = with pkgs; [
+        intel-media-driver
+        vaapi
+        Intel
+        vaapi
+        Vdpau
+        lib
+        vdpau-va-gl
+      ];
       driSupport = true;
       driSupport32Bit = true;
     };
