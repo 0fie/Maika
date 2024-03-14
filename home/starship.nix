d5c182054e4cc8051654e0d703e05afcd834deae
0fie <151028199+0fie@users.noreply.github.com>
2024-03-15 01:25:37 +0200

    catppuccin starship - now declared properly

--- home/starship.nix
+++ home/starship.nix
@@ -1,8 +1,41 @@
-{ lib, inputs, ... }:
+{ inputs, ... }:
 
 {
   programs.starship = {
     enable = true;
-    settings = lib.importTOML "${inputs.trash}/starship.toml";
+    enableNushellIntegration = true;
+
+    settings = {
+      right_format = "$cmd_duration";
+
+      directory = {
+        format = "[ ](bold #89b4fa)[ $path ]($style)";
+        style = "bold #b4befe";
+      };
+
+      character = {
+        success_symbol = "[ ](bold #89b4fa)[ ➜](bold green)";
+        error_symbol = "[ ](bold #89b4fa)[ ➜](bold red)";
+        # error_symbol = "[ ](bold #89dceb)[ ✗](bold red)";
+      };
+
+      cmd_duration = {
+        format =
+          "[]($style)[[󰔚 ](bg:#161821 fg:#d4c097 bold)$duration](bg:#161821 fg:#BBC3DF)[ ]($style)";
+        disabled = false;
+        style = "bg:none fg:#161821";
+      };
+
+      directory.substitutions = {
+        "~" = "󰋞";
+        "Documents" = " ";
+        "Downloads" = " ";
+        "Music" = " ";
+        "Pictures" = " ";
+      };
+
+      palette = "catppuccin_mocha";
+    } // builtins.fromTOML
+      (builtins.readFile "${inputs.catppuccin-starship}/palettes/mocha.toml");
   };
 }
