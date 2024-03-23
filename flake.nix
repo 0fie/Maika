a6460144317e456005a17cfbca9c4ade08711f46
0fie <151028199+0fie@users.noreply.github.com>
2024-03-23 22:07:40 +0200

    removed nix-community cachix

--- flake.nix
+++ flake.nix
@@ -99,11 +99,8 @@
     };
 
   nixConfig = {
-    extra-substituters =
-      [ "https://hyprland.cachix.org" "https://nix-community.cachix.org" ];
-    extra-trusted-public-keys = [
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
+    extra-substituters = [ "https://hyprland.cachix.org" ];
+    extra-trusted-public-keys =
+      [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
   };
 }
