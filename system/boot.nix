af1a29a88dff03cf856d47f19a830092bfc819fd
0fie <151028199+0fie@users.noreply.github.com>
2024-03-24 19:34:43 +0200

    fix: temporarily removed plymouth

--- system/boot.nix
+++ system/boot.nix
@@ -1,20 +1,11 @@
-{ pkgs, inputs, ... }:
+{ ... }:
 
-let inherit (import ../home/options.nix) userName;
-in {
+{
   boot = {
     loader.grub = {
       enable = true;
       device = "/dev/sda";
       useOSProber = true;
-    };
-    plymouth = {
-      enable = true;
-      #font = "${inputs.trash}/fonts/Regular.ttf";
-      font =
-        "${pkgs.jetbrains-mono}/share/fonts/truetype/JetBrainsMono-Regular.ttf";
-      themePackages = [ pkgs.catppuccin-plymouth ];
-      theme = "catppuccin-macchiato";
     };
   };
 }
