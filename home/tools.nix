1b3a36d128f1df87588a3baebc7b3d4fd36db580
0fie <151028199+0fie@users.noreply.github.com>
2024-03-25 17:23:24 +0200

    feat: added gcc for neovim

--- home/tools.nix
+++ home/tools.nix
@@ -3,18 +3,19 @@
 {
   # Packages that should be installed to the user profile.
   home.packages = with pkgs; [
+    gcc
     gimp
     git-extras # Provides useful commands like git-summary
     iotop
     killall
     libreoffice
     loupe # image viewer
+    mpv-unwrapped
     persepolis # download manager
     protonvpn-gui
     shotcut # lightweight video cutter
     soundwireserver # cast sound to Android phone.
     transmission-gtk # torrent client
-    vlc
     vokoscreen-ng # screen recorder
     which
   ];
