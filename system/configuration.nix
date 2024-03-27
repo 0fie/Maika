0b0250f5ecb9c798499615d0425910e60d490e02
0fie <151028199+0fie@users.noreply.github.com>
2024-03-27 10:53:06 +0200

    feat: adding luks encryption

--- system/configuration.nix
+++ system/configuration.nix
@@ -16,6 +16,13 @@
     ./sys.nix
     ./users.nix
   ];
+  # Setup keyfile
+  boot.initrd.secrets = {
+    "/crypto_keyfile.bin" = null;
+  };
+
+  boot.loader.grub.enableCryptodisk=true;
+  boot.initrd.luks.devices."luks-ad25b88c-d338-4d0f-a1f6-145c0c451cc0".keyFile = "/crypto_keyfile.bin";
 
   # Do not change this unless you truly understand what you're doing. It's actually fine as it is.
   system.stateVersion = "23.05";
