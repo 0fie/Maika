08d515186a443a3ec89320808b2a469106da5f50
0fie <151028199+0fie@users.noreply.github.com>
2024-03-29 17:36:18 +0200

    chore: removed unused cli tools

--- home/tools.nix
+++ home/tools.nix
@@ -6,8 +6,6 @@
     #charm-freeze
     gimp # for making memes. Please suggest a lighter alternative.
     git-extras # Provides useful commands like git-summary
-    iotop
-    killall
     libreoffice
     loupe # image viewer
     mpv-unwrapped # lightweight media player
@@ -16,7 +14,6 @@
     soundwireserver # cast sound to Android phone.
     transmission-gtk # torrent client
     vokoscreen-ng # screen recorder (buggy on Wayland)
-    which
 
     inputs.catppuccinifier.packages.${pkgs.system}.cli
   ];
