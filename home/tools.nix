d2523c7a7525d4f0a73a9ed455df1c7b2a604e04
0fie <151028199+0fie@users.noreply.github.com>
2024-03-29 15:34:35 +0200

    chore: removed 2 unused apps

--- home/tools.nix
+++ home/tools.nix
@@ -4,20 +4,18 @@
   # Packages that should be installed to the user profile.
   home.packages = with pkgs; [
     #charm-freeze
-    gcc
-    gimp
+    gimp # for making memes. Please suggest a lighter alternative.
     git-extras # Provides useful commands like git-summary
     iotop
     killall
     libreoffice
     loupe # image viewer
-    mpv-unwrapped
-    persepolis # download manager
+    mpv-unwrapped # lightweight media player
     protonvpn-gui
     shotcut # lightweight video cutter
     soundwireserver # cast sound to Android phone.
     transmission-gtk # torrent client
-    vokoscreen-ng # screen recorder
+    vokoscreen-ng # screen recorder (buggy on Wayland)
     which
 
     inputs.catppuccinifier.packages.${pkgs.system}.cli
