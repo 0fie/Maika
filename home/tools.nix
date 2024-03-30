fe0cc0277bf31cea3176dda4e47638bf15b28f04
0fie <151028199+0fie@users.noreply.github.com>
2024-03-30 12:10:11 +0200

    chore: removed unused apps

--- home/tools.nix
+++ home/tools.nix
@@ -1,18 +1,15 @@
 { pkgs, inputs, ... }:
 
 {
-  # Packages that should be installed to the user profile.
+  # Additional packages that should be installed to the user profile.
   home.packages = with pkgs; [
     #charm-freeze
-    gimp # for making memes. Please suggest a lighter alternative.
     git-extras # Provides useful commands like git-summary
-    libreoffice
+    libreoffice # Is there a lighter alternative?
     loupe # image viewer
     mpv-unwrapped # lightweight media player
     protonvpn-gui
-    shotcut # lightweight video cutter
     soundwireserver # cast sound to Android phone.
-    transmission-gtk # torrent client
     vokoscreen-ng # screen recorder (buggy on Wayland)
 
     inputs.catppuccinifier.packages.${pkgs.system}.cli
