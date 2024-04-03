786a518f6903743203b36f63de5bdaad3079e461
0fie <151028199+0fie@users.noreply.github.com>
2024-04-03 14:44:53 +0200

    fix: keep build outputs for offline rebuilds

--- system/nix-settings.nix
+++ system/nix-settings.nix
@@ -1,19 +1,27 @@
-{ inputs, lib, ... }:
-
+{
+  inputs,
+  lib,
+  ...
+}:
 # Fetch the userName from our home/options.nix file. It will be used to add the user to Nix's "trusted-users" so that we can
 # have additional rights when interacting with the Nix daemon.
-let inherit (import ../home/options.nix) userName;
-
+let
+  inherit (import ../home/options.nix) userName;
 in {
   nix = {
+    # Keep build-time dependencies around to be able to rebuild while being offline.
+    extraOptions = ''
+      keep-outputs = true
+      keep-derivations = true
+    '';
     # Pin the registry to avoid downloading and evaling a new nixpkgs version every time.
     # I can not verify whether this works as intended.
-    registry = lib.mapAttrs (_: v: { flake = v; }) inputs;
+    registry = lib.mapAttrs (_: v: {flake = v;}) inputs;
 
     settings = {
       auto-optimise-store = true;
-      trusted-users = [ "${userName}" ];
-      experimental-features = [ "nix-command" "flakes" ];
+      trusted-users = ["${userName}"];
+      experimental-features = ["nix-command" "flakes"];
     };
 
     # Enable auto cleanup.
@@ -26,5 +34,5 @@ in {
     };
   };
 
-  nixpkgs.config = { allowUnfree = true; };
+  nixpkgs.config = {allowUnfree = true;};
 }
