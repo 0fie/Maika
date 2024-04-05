b4451258b023bcafae98d07be94bf58ad0d61727
0fie <151028199+0fie@users.noreply.github.com>
2024-04-05 13:25:58 +0200

    added catppuccin grub theme

--- system/boot.nix
+++ system/boot.nix
@@ -1,9 +1,11 @@
+{inputs, ...}:
 # This is kept in a separate file to allow you to copy the system/sys.nix file as it is.
 {
   boot = {
     loader.grub = {
       enable = true;
       device = "/dev/sda";
+      theme = inputs.catppuccin-grub + "/src/catppuccin-mocha-grub-theme";
     };
 
     kernel.sysctl = {
