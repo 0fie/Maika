9007d163b60337c49834bb080223481f574b699f
0fie <151028199+0fie@users.noreply.github.com>
2024-04-07 08:01:54 +0200

    chore: removed useless GST plugins and nautilus.nix

--- home/services.nix
+++ home/services.nix
@@ -7,6 +7,13 @@
   scripts = import ./scripts.nix {inherit pkgs;};
 in {
   services = {
+    udiskie = {
+      enable = true;
+      automount = true;
+      notify = true;
+      tray = "never";
+    };
+
     network-manager-applet.enable = true;
     blueman-applet.enable = true;
 
