cae64857b041e6f761f376e9a0442e02ee8a38ec
0fie <151028199+0fie@users.noreply.github.com>
2024-04-08 09:47:09 +0200

    fix: icons and fonts are now managed imperatively for some valid reason

--- home/gtk.nix
+++ home/gtk.nix
@@ -1,7 +1,6 @@
 {
   config,
   pkgs,
-  inputs,
   ...
 }: {
   gtk = {
@@ -25,16 +24,11 @@
   };
 
   # Symlink the `~/.config/gtk-4.0/` folder declaratively to theme GTK-4 apps as well.
-  xdg.configFile = {
-    "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
-    "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
-    "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
+  xdg.configFile = let
+    g = config.gtk.theme.package;
+  in {
+    "gtk-4.0/assets".source = "${g}/share/themes/${g}/gtk-4.0/assets";
+    "gtk-4.0/gtk.css".source = "${g}/share/themes/${g}/gtk-4.0/gtk.css";
+    "gtk-4.0/gtk-dark.css".source = "${g}/share/themes/${g}/gtk-4.0/gtk-dark.css";
   };
-
-  # The bibata-cursors package in nixpkgs downloads over 400MB worth of assets. I only need the 2MB "modern-ice".
-  # I don't know if or how the package can be overriden. So this is a quick hack to save my time and bandwidth.
-  home.file.".icons/default" = {
-    source = "${inputs.trash}/My-Bibata-Modern-Ice";
-  };
-  home.file.".local/share/fonts" = {source = "${inputs.trash}/fonts";};
 }
