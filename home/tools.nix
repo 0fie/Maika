69955c2b8d002b7e1079f34c14db3a785bc6e637
0fie <151028199+0fie@users.noreply.github.com>
2024-04-08 09:52:14 +0200

    chore: removed unused burpsuite, libreoffice & catppuccinifier

--- home/tools.nix
+++ home/tools.nix
@@ -5,18 +5,15 @@
 }: {
   # Additional packages that should be installed to the user profile.
   home.packages = with pkgs; [
-    burpsuite # to be replaced with Postman.
     #charm-freeze
     jetbrains-toolbox
     git-extras # Provides useful commands like git-summary
-    libreoffice # Is there a lighter alternative?
     loupe # image viewer
     mpv-unwrapped # lightweight media player
     protonvpn-gui # at least it's open source
     soundwireserver # cast sound to Android phone.
-    vokoscreen-ng # screen recorder (buggy on Wayland)
+    vokoscreen-ng # screen recorder (extremely buggy on Wayland)
 
-    inputs.catppuccinifier.packages.${pkgs.system}.cli # Applies Catppuccin flavors to images.
     inputs.mika.packages.${system}.default # my neovim flake.
   ];
 }
