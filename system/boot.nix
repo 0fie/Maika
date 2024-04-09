56d4b1ebecefa17235269e5336befb4198ca4c6a
0fie <151028199+0fie@users.noreply.github.com>
2024-04-09 06:04:59 +0200

    catppuccin.enable

--- system/boot.nix
+++ system/boot.nix
@@ -1,17 +1,10 @@
-{pkgs, ...}: {
+{
   boot = {
     loader.grub = {
       enable = true;
       device = "/dev/sda";
       timeoutStyle = "hidden";
-      theme =
-        pkgs.fetchFromGitHub {
-          owner = "catppuccin";
-          repo = "grub";
-          rev = "88f6124757331fd3a37c8a69473021389b7663ad";
-          hash = "sha256-e8XFWebd/GyX44WQI06Cx6sOduCZc5z7/YhweVQGMGY=";
-        }
-        + "/src/catppuccin-mocha-grub-theme";
+      catppuccin.enable = true;
     };
 
     kernel.sysctl = {
