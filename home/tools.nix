3dc0bc1fb587bd97791f9d5280e392957e0f8eed
0fie <151028199+0fie@users.noreply.github.com>
2024-04-15 15:25:17 +0200

    added charm-freeze, removed vokoscreen-ng

--- home/tools.nix
+++ home/tools.nix
@@ -5,14 +5,13 @@
 }: {
   # Additional packages that should be installed to the user profile.
   home.packages = with pkgs; [
-    # charm-freeze
+    charm-freeze
     jetbrains-toolbox
     git-extras # Provides useful commands like git-summary
     loupe # image viewer
     mpv-unwrapped # lightweight media player
     gnome.nautilus # GUI file manager
     protonvpn-gui # at least it's open source
-    vokoscreen-ng # screen recorder (extremely buggy on Wayland)
 
     inputs.mika.packages.${pkgs.system}.default # my neovim flake
   ];
