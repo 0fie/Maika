fc71dbe7d74f90a72025f09a9733489aa1c8e5b8
0fie <151028199+0fie@users.noreply.github.com>
2024-04-17 12:23:45 +0200

    added nix-inspect

--- home/tools.nix
+++ home/tools.nix
@@ -7,11 +7,12 @@
   home.packages = with pkgs; [
     charm-freeze
     exercism
+    gnome.nautilus # GUI file manager
     jetbrains-toolbox
     git-extras # Provides useful commands like git-summary
     loupe # image viewer
     mpv-unwrapped # lightweight media player
-    gnome.nautilus # GUI file manager
+    nix-inspect # Interactive TUI for inspecting nix configs.
     protonvpn-gui # at least it's open source
 
     inputs.mika.packages.${pkgs.system}.default # my neovim flake
