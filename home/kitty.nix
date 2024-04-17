6bdb7ce60696e3bd8f9aab6ba7c87ff1a9b7a257
0fie <151028199+0fie@users.noreply.github.com>
2024-04-17 19:31:33 +0200

    removed unused bash integration

--- home/kitty.nix
+++ home/kitty.nix
@@ -7,19 +7,16 @@ in {
   programs.kitty = {
     enable = true;
     catppuccin.enable = true;
-    shellIntegration.enableBashIntegration = true;
-    font.name = "${fontName}";
-    font.size = 11;
+    font = {
+      name = "${fontName}";
+      size = 11;
+    };
     settings = {
-      background_opacity = "1";
+      background_opacity = "1"; # Hyprland will set a uniform opacity for all apps. See home/Hyprland.nix
       enable_audio_bell = "no";
-
       cursor_text_color = "background";
-
       window_padding_width = 4;
-
       listen_on = "unix:/tmp/kitty";
-      shell_integration = "enabled";
     };
   };
 }
